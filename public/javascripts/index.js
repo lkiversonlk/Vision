bf6ec15394336f4b004c011cbbf93d948af822a3
lkiversonlk <city.of.beijing@gmail.com>
2015-07-07 19:54:37 +0800

    use jade in the client side to render web

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,21 @@
  * Created by kliu on 07/07/2015.
  */
 
-$(".icon").on('click', function(){
-    $(".icon.active").removeClass("active");
-    $(this).addClass("active");
+
+var fake_puttings = [
+    {
+        name : "test1"
+    },
+    {
+        name : "test2"
+    },
+    {
+        name : "test3"
+    }
+];
+
+var fn = jade.compile("div\n    | #{hello}");
+var html = fn({
+    "hello" : "this is jsut a test"
 });
+alert(html);
